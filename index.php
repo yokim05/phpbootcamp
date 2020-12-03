1db1f55457102b124b379e0ff0dba89a912d9da8
dhika roid <dhikayokim@gmail.com>
2020-12-03 14:50:17 +0700

    init

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+<?php include_once "init/template/header.php" ?>
+
+
+<div class="container">
+
+    <h1 class="text-center">HTML</h1>
+    <hr style="border-top: 1px solid black;">
+
+    <ul>
+        <li><a href="pages/html/firsthtml.php" class="text-dark">firsthtml</a></li>
+        <li><a href="#" class="text-dark">test</a></li>
+        <li><a href="#" class="text-dark">test</a></li>
+        <li><a href="#" class="text-dark">test</a></li>
+        <li><a href="#" class="text-dark">test</a></li>
+    </ul>
+</div>
+
+
+
+<?php include_once "init/template/footer.php" ?>
